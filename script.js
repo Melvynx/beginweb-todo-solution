d875419dafc163183a1bec11944ad2fa534e5382
melvynxdev <melvynmal@gmail.com>
2024-11-26 09:40:27 +0800

    feat(bg-img): reorder background images

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const BG_IMG_KEY = "beginweb-bg-image";
 const backgroundImages = [
   "/images/background-2.jpg",
   "/images/background-1.jpg",
-  "/images/background-4.jpg",
   "/images/background-3.jpg",
+  "/images/background-4.jpg",
 ];
 
 backgroundImages.forEach((img) => {
